fe408062f4562ed69ca2564710651e6594e44c2f
Dima Kachurovskyy <mekaa2.718@gmail.com>
2021-11-07 10:50:12 +0200

    added python venv; added python docker image; added make file execution commands

--- trainning_data/jesus_bot.py
+++ trainning_data/jesus_bot.py
@@ -1,3 +1,33 @@
+import logging
+import os
+
 from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler,  MessageHandler, Filters
 
 load_dotenv()
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+updater = Updater(token=os.environ.get('TOKEN'), use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a Jesus bot!")
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Sorry, I didn't understand that command.")
+
+
+start_handler = CommandHandler('start', start)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(unknown_handler)
+
+updater.start_polling()
+updater.idle()
