3f8b9b7ab67e8c0baf69a3b376fe7eca80c884bc
Olga Ozhogina <ilonmusk7777777@gmail.com>
2021-11-09 22:44:17 +0300

    aiogram example

--- trainning_data/jesus_bot.py
+++ trainning_data/jesus_bot.py
@@ -2,32 +2,25 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler,  MessageHandler, Filters
+from aiogram import Bot, Dispatcher, executor, types
 
 load_dotenv()
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-updater = Updater(token=os.environ.get('TOKEN'), use_context=True)
-dispatcher = updater.dispatcher
+bot = Bot(token=os.environ.get('TOKEN'))
+dp = Dispatcher(bot)
 
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+   
+    await message.reply("Hi! I'm Jesus_bot!I love you")
 
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a Jesus bot!")
+@dp.message_handler()
+async def echo(message: types.Message):
 
+    await message.answer(message.text)
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Sorry, I didn't understand that command.")
-
-
-start_handler = CommandHandler('start', start)
-unknown_handler = MessageHandler(Filters.command, unknown)
-
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(unknown_handler)
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
